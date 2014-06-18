fb4f7020d02f244645a3e89b54b0e2a35066d308
prston <a.kraft@foxel.ch>
2014-06-18 10:48:18 +0200

    Integrate Piwik Open Analytics Platform

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -181,5 +181,19 @@
     </div>
 </footer>
 
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://foxel.ch/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="http://foxel.ch/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+
 </body>
 </html>
